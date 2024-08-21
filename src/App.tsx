84f2952e7b745f6e005a036f46d3cfaf089c17e8
Jonh Alexis Buot <ja.buotph@gmail.com>
2024-08-21 16:26:14 +0800

    feat(io): moved out io operations from App

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { clsx } from "clsx";
 import { useCallback, useEffect, useRef, useState } from "react";
 
 import type { Line } from "./tsv/types";
+import { parseTsvFile } from "./tsv/io";
 import {
   incrementIndent,
   decrementIndent,
@@ -83,7 +84,18 @@ export default function App() {
 
   return (
     <div className="container m-auto">
-      <TSVFileUpload onUpload={(lines) => setLines(calculateIndent(lines))} />
+      <input
+        type="file"
+        accept=".tsv"
+        onChange={async (e) => {
+          const file = e.target.files?.[0];
+
+          if (file) {
+            const lines = await parseTsvFile(file);
+            setLines(calculateIndent(lines));
+          }
+        }}
+      />
 
       <ul className="container py-12">
         {lines.map((line, i) => (
@@ -132,50 +144,5 @@ function CarriageReturn({
       <polyline points="9 10 4 15 9 20" />
       <path d="M20 4v7a4 4 0 0 1-4 4H4" />
     </svg>
-  );
-}
-
-function TSVFileUpload({ onUpload }: { onUpload: (lines: Line[]) => void }) {
-  return (
-    <input
-      type="file"
-      accept=".tsv"
-      onChange={(e) => {
-        const file = e.target.files?.[0];
-
-        if (file) {
-          const reader = new FileReader();
-
-          reader.onload = (e) => {
-            if (e.target && typeof e.target.result === "string") {
-              const lines = e.target.result
-                // Split the text into lines.
-                .split("\n")
-                // Remove "/r/n" from the end of each line.
-                .map((line) => line.trimEnd())
-                // Remove empty lines.
-                .filter((line) => !!line)
-                // Convert each line into an object.
-                .map((line) => {
-                  // Split the line by tabs to get the text, pointer, and label.
-                  const [text, pointer, label] = line.split("\t");
-
-                  return {
-                    text,
-                    pointer: parseInt(pointer),
-                    label: label as Line["label"],
-                    indent: 0,
-                    parent: -1,
-                  };
-                });
-
-              onUpload(lines);
-            }
-          };
-
-          reader.readAsText(file);
-        }
-      }}
-    />
   );
 }
