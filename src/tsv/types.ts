d556dcf39c94138def82ed586d2259cf597d60c6
Jonh Alexis Buot <ja.buotph@gmail.com>
2024-08-21 18:22:17 +0800

    chore(doc): added documentation for tsv line type

--- src/tsv/types.ts
+++ src/tsv/types.ts
@@ -1,7 +1,26 @@
 export type Line = {
+  /**
+   * The text content of the Line
+   */
   text: string;
-  pointer: number; // 1-indexed, -1 for root, 0 for unset
-  parent: number; // 0-indexed, -1 for root
-  label: "c" | "d" | "s" | "";
+  /**
+   * Tells the line to where the next line is nested under as an indent block.
+   * This is 1-indexed. -1 for root, 0 for unset.
+   */
+  pointer: number;
+  /**
+   * The transition label of the line to the next line.
+   * For full explanation, see:
+   * https://github.com/stanfordnlp/pdf-struct?tab=readme-ov-file#annotating-tsv-files
+   */
+  label: "c" | "d" | "s" | "x" | "e" | "";
+  /**
+   * The indentation level of the line.
+   */
   indent: number;
+  /**
+   * Internal variable to keep track of the parent of the current line in an indent block.
+   * This is 0-indexed, -1 is used for root.
+   */
+  parent: number;
 };
